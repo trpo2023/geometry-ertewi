0da7a11d02dc4af87393283f0f05e04f98f12db5
ertewi <nikita28984@gmail.com>
2023-02-15 11:21:14 +0700

    Start writing programm

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    FILE* tfile;
+
+    tfile = fopen("input", "r");
+
+    if (tfile == NULL) {
+        printf("Can`t open file\n");
+        return 1;
+    } else {
+        printf("File opened succesfully\n\n");
+    }
+    
+    char *str[100];
+    fgets(*str, 99, tfile);
+    printf("%s\n", *str);
+
+    fclose(tfile);
+    printf("File closed\n");
+    printf("Exit program...\n");
     return 0;
 }
