dd72ef529caeaf06afa35e9322bb3507c5188b55
ertewi <nikita28984@gmail.com>
2023-03-10 17:07:40 +0700

    add circle-check

--- main.c
+++ main.c
@@ -15,12 +15,18 @@ int main(int argc, char *argv[])
         printf("File opened succesfully\n\n");
     }
     
-    char *str[100];
-    fgets(*str, 99, tfile);
-    printf("%s\n", *str);
+    char str[100 + 1];
+    fgets(str, 100, tfile);
+    printf("%s\n", str);
+
+    if(strncmp(str, "circle", 6) == 0) {
+        printf("have circle\n");
+    } else {
+        printf("^\nError at column 0: expected 'circle'\n");
+    }
 
     fclose(tfile);
-    printf("File closed\n");
+    printf("\nFile closed\n");
     printf("Exit program...\n");
     return 0;
 }
