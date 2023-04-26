d0c85f460b019ead46736c204c9f058654c004c6
ertewi <nikita28984@gmail.com>
2023-04-27 01:43:50 +0700

    test geometry suit

--- src/libgeometry/geometrylib.c
+++ src/libgeometry/geometrylib.c
@@ -4,10 +4,16 @@
 
 float count_perimeter(float rad)
 {
-    return (2 * M_PI * rad);
+    if (rad > 0)
+        return (2 * M_PI * rad);
+
+    return 0;
 }
 
 float count_area(float rad)
 {
-    return (M_PI * rad * rad);
+    if (rad > 0)
+        return (M_PI * rad * rad);
+
+    return 0;
 }
--- test/tests.c
+++ test/tests.c
@@ -2,8 +2,23 @@
 #include <libgeometry/parser.h>
 
 #include <ctest.h>
+#include <stdio.h>
 
-CTEST(geometry_suite, perimeter)
+CTEST(geometry_suite, perimeter_1)
+{
+    // Given
+    const float a = 1.5;
+
+    // When
+    const float result = count_perimeter(a);
+
+    // Then
+    const float expected = 9.424778;
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(geometry_suite, perimeter_2)
 {
     // Given
     const float a = 1;
@@ -12,6 +27,95 @@ CTEST(geometry_suite, perimeter)
     const float result = count_perimeter(a);
 
     // Then
-    const float expected = count_perimeter(a);
+    const float expected = 6.283185;
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(geometry_suite, perimeter_3)
+{
+    // Given
+    const float a = 0;
+
+    // When
+    const float result = count_perimeter(a);
+
+    // Then
+    const float expected = 0;
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(geometry_suite, perimeter_4)
+{
+    // Given
+    const float a = -1;
+
+    // When
+    const float result = count_perimeter(a);
+
+    // Then
+    const float expected = 0;
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(geometry_suite, area_1)
+{
+    // Given
+    const float a = -1;
+
+    // When
+    const float result = count_area(a);
+
+    // Then
+    const float expected = 0;
+
+    printf("%f", count_area(a));
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(geometry_suite, area_2)
+{
+    // Given
+    const float a = 0;
+
+    // When
+    const float result = count_area(a);
+
+    // Then
+    const float expected = 0;
+
+    printf("%f", count_area(a));
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(geometry_suite, area_3)
+{
+    // Given
+    const float a = 1;
+
+    // When
+    const float result = count_area(a);
+
+    // Then
+    const float expected = 3.141593;
+
+    printf("%f", count_area(a));
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(geometry_suite, area_4)
+{
+    // Given
+    const float a = 1.5;
+
+    // When
+    const float result = count_area(a);
+
+    // Then
+    const float expected = 7.068583;
+
+    printf("%f", count_area(a));
     ASSERT_EQUAL(expected, result);
 }
