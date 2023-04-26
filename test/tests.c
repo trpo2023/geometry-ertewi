c18d31d1ac26c565ba60a42736844f1faf177950
ertewi <nikita28984@gmail.com>
2023-04-27 02:05:11 +0700

    covered with tests

--- test/tests.c
+++ test/tests.c
@@ -3,6 +3,7 @@
 
 #include <ctest.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 CTEST(geometry_suite, perimeter_1)
 {
@@ -71,7 +72,6 @@ CTEST(geometry_suite, area_1)
     // Then
     const float expected = 0;
 
-    printf("%f", count_area(a));
     ASSERT_EQUAL(expected, result);
 }
 
@@ -86,7 +86,6 @@ CTEST(geometry_suite, area_2)
     // Then
     const float expected = 0;
 
-    printf("%f", count_area(a));
     ASSERT_EQUAL(expected, result);
 }
 
@@ -101,7 +100,6 @@ CTEST(geometry_suite, area_3)
     // Then
     const float expected = 3.141593;
 
-    printf("%f", count_area(a));
     ASSERT_EQUAL(expected, result);
 }
 
@@ -116,6 +114,149 @@ CTEST(geometry_suite, area_4)
     // Then
     const float expected = 7.068583;
 
-    printf("%f", count_area(a));
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(parser_suite, test_1)
+{
+    // Given
+    char str[100 + 1] = "circle(1 2, 3)";
+    char* err_msg;
+    int err_smb;
+    char* c_radius;
+    char* c_x;
+    char* c_y;
+
+    c_radius = malloc(100 * sizeof(char));
+    c_x = malloc(100 * sizeof(char));
+    c_y = malloc(100 * sizeof(char));
+
+    // When
+    const int result
+            = find_circle(str, &err_msg, &err_smb, &c_radius, &c_x, &c_y);
+
+    // Then
+    const int expected = 0;
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(parser_suite, test_2)
+{
+    // Given
+    char str[100 + 1] = "circle(1 2 3)";
+    char* err_msg;
+    int err_smb;
+    char* c_radius;
+    char* c_x;
+    char* c_y;
+
+    c_radius = malloc(100 * sizeof(char));
+    c_x = malloc(100 * sizeof(char));
+    c_y = malloc(100 * sizeof(char));
+
+    // When
+    const int result
+            = find_circle(str, &err_msg, &err_smb, &c_radius, &c_x, &c_y);
+
+    // Then
+    const int expected = 1;
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(parser_suite, test_3)
+{
+    // Given
+    char str[100 + 1] = "circle(1 2, 3";
+    char* err_msg;
+    int err_smb;
+    char* c_radius;
+    char* c_x;
+    char* c_y;
+
+    c_radius = malloc(100 * sizeof(char));
+    c_x = malloc(100 * sizeof(char));
+    c_y = malloc(100 * sizeof(char));
+
+    // When
+    const int result
+            = find_circle(str, &err_msg, &err_smb, &c_radius, &c_x, &c_y);
+
+    // Then
+    const int expected = 1;
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(parser_suite, test_4)
+{
+    // Given
+    char str[100 + 1] = "circle(1 2, )";
+    char* err_msg;
+    int err_smb;
+    char* c_radius;
+    char* c_x;
+    char* c_y;
+
+    c_radius = malloc(100 * sizeof(char));
+    c_x = malloc(100 * sizeof(char));
+    c_y = malloc(100 * sizeof(char));
+
+    // When
+    const int result
+            = find_circle(str, &err_msg, &err_smb, &c_radius, &c_x, &c_y);
+
+    // Then
+    const int expected = 1;
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(parser_suite, test_5)
+{
+    // Given
+    char str[100 + 1] = "circle(-1.7 -2.1, 3.5)";
+    char* err_msg;
+    int err_smb;
+    char* c_radius;
+    char* c_x;
+    char* c_y;
+
+    c_radius = malloc(100 * sizeof(char));
+    c_x = malloc(100 * sizeof(char));
+    c_y = malloc(100 * sizeof(char));
+
+    // When
+    const int result
+            = find_circle(str, &err_msg, &err_smb, &c_radius, &c_x, &c_y);
+
+    // Then
+    const int expected = 0;
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(parser_suite, test_6)
+{
+    // Given
+    char str[100 + 1] = "";
+    char* err_msg;
+    int err_smb;
+    char* c_radius;
+    char* c_x;
+    char* c_y;
+
+    c_radius = malloc(100 * sizeof(char));
+    c_x = malloc(100 * sizeof(char));
+    c_y = malloc(100 * sizeof(char));
+
+    // When
+    const int result
+            = find_circle(str, &err_msg, &err_smb, &c_radius, &c_x, &c_y);
+
+    // Then
+    const int expected = 1;
+
     ASSERT_EQUAL(expected, result);
 }
